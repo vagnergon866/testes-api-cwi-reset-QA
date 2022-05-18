a6921c2c54d0cf20599b8e1b6ee9c93d1302d181
Vagner Gonçalves <88849441+vagnergon866@users.noreply.github.com>
2022-05-18 15:16:49 -0300

    teste verifica api online

---
+++ restassureapitest/src/test/java/ClasseDeTeste.java
@@ -0,0 +1,24 @@
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.lessThan;
+
+public class ClasseDeTeste {
+
+    @Test
+    public void validaApiOnline(){
+        Response responsePing =
+                        given()
+                        .header("Content-Type", "application/json")
+                        .when()
+                        .get("https://treinamento-api.herokuapp.com/ping");
+
+        responsePing
+                .then()
+                .statusCode(201)
+                .time(lessThan(2L), TimeUnit.SECONDS);
+    }
+}
