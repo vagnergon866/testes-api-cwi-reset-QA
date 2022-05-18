1223d71f62c16343f5e73ef06ac191608b3c928f
Vagner Gonçalves <88849441+vagnergon866@users.noreply.github.com>
2022-05-18 15:39:55 -0300

    teste valida listagem de reservas

--- restassureapitest/src/test/java/ClasseDeTeste.java
+++ restassureapitest/src/test/java/ClasseDeTeste.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
 
 public class ClasseDeTeste {
@@ -21,4 +22,19 @@ public class ClasseDeTeste {
                 .statusCode(201)
                 .time(lessThan(2L), TimeUnit.SECONDS);
     }
+
+    @Test
+    public void validaListagensDeIdsDasReservas(){
+        Response responseListagemIdsReservas =
+                        given()
+                        .when()
+                        .get("https://treinamento-api.herokuapp.com/booking");
+
+        responseListagemIdsReservas
+                .then()
+                .statusCode(200)
+                .body("size()", greaterThan(0));
+
+    }
+
 }
